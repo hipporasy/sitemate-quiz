dc19e8cb1616f1532b72051aa0556969a267e90c
Marasy Phi <hipporasy@gmail.com>
2022-07-07 11:17:31 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {FlatList, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {useEffect, useState} from "react";
 
 export default function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getTodoListApi();
+  }, [])
+
+
+  const getTodoListApi = () => {
+    return fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => {
+        setItems(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
+      />
+      <StatusBar style="auto"/>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
